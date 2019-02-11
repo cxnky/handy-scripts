d0bceda50f38050107f4dc1c9575b459ca24ab08
Connor Wright <connor@connorwright.uk>
2019-02-11 02:08:15 +0000

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -2,7 +2,7 @@
 apt update 
 apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
 apt-get install docker-ce
 echo Docker installed successfully
